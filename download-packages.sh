4f202a791faa0f6a221779f47e904b19a17e0a06
Baurzhan Muftakhidinov <baurthefirst@gmail.com>
2012-02-16 19:22:34 +0600

    Update inxi package to 1.7.28.

--- download-packages.sh
+++ download-packages.sh
@@ -15,7 +15,7 @@ PACKAGES=(	'http://dl.dropbox.com/u/5626215/live-installer_2012.01.24~kazsid0_al
 		'http://ppa.launchpad.net/tiheum/equinox/ubuntu/pool/main/f/faenza-icon-theme/faenza-icon-theme_1.2_all.deb'
 		'http://ppa.launchpad.net/webupd8team/themes/ubuntu/pool/main/f/faenza-colors/faenza-cupertino_0.3.4-1~webupd8~oneiric_all.deb'
 		'http://files.baurzhan.info/ttf-microsoft-corefonts_1-1_all.deb'
-		'http://files.baurzhan.info/inxi_1.4.23-1_all.deb'
+		'http://files.baurzhan.info/inxi_1.7.28-1_all.deb'
 		'http://ppa.launchpad.net/ubuntu-wine/ppa/ubuntu/pool/main/w/wine1.3/wine1.3_1.3.37-0ubuntu1~ppa1~oneiric1_i386.deb'
 		'http://linukz.org/debian/pool/main/s/stardict-kazrus/stardict-kazrus_20100827-1_all.deb'
 		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flash-properties-gtk_11.1.102.55-0oneiric1_i386.deb'
