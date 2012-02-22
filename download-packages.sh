52609dde4a3e1fe18c0b74a72f68114def1dc8ae
Baurzhan Muftakhidinov <baurthefirst@gmail.com>
2012-02-22 14:55:32 +0600

    Package updates: live-installer, wine 1.4 RC4.

--- download-packages.sh
+++ download-packages.sh
@@ -7,13 +7,13 @@
 #################################################################################
 
 DIRECTORY="config/packages.chroot"
-PACKAGES=(	'http://dl.dropbox.com/u/5626215/live-installer_2012.01.24~kazsid0_all.deb'
+PACKAGES=(	'http://dl.dropbox.com/u/5626215/live-installer_2012.02.21~kazsid0_all.deb'
 		'http://dl.dropbox.com/u/5626215/deadbeef_0.5.1-1_i386.deb'
 		'http://ppa.launchpad.net/tiheum/equinox/ubuntu/pool/main/f/faenza-icon-theme/faenza-icon-theme_1.2_all.deb'
 		'http://ppa.launchpad.net/webupd8team/themes/ubuntu/pool/main/f/faenza-colors/faenza-cupertino_0.3.4-1~webupd8~oneiric_all.deb'
 		'http://files.baurzhan.info/ttf-microsoft-corefonts_1-1_all.deb'
 		'http://files.baurzhan.info/inxi_1.7.28-1_all.deb'
-		'http://ppa.launchpad.net/ubuntu-wine/ppa/ubuntu/pool/main/w/wine1.3/wine1.3_1.3.37-0ubuntu1~ppa1~oneiric1_i386.deb'
+		'http://ppa.launchpad.net/ubuntu-wine/ppa/ubuntu/pool/main/w/wine1.3/wine1.3_1.4~rc4-0ubuntu1_i386.deb'
 		'http://linukz.org/debian/pool/main/s/stardict-kazrus/stardict-kazrus_20100827-1_all.deb'
 		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flash-properties-gtk_11.1.102.62-0oneiric1_i386.deb'
 		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flashplugin_11.1.102.62-0oneiric1_i386.deb'
