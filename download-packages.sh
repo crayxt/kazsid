52bece6699a19e0529cfd0a3eead0bbb270d7127
Baurzhan Muftakhidinov <baurthefirst@gmail.com>
2012-05-07 14:53:59 +0600

    Update flash player download links.

--- download-packages.sh
+++ download-packages.sh
@@ -15,8 +15,8 @@ PACKAGES=(	'http://dl.dropbox.com/u/5626215/live-installer_2012.02.21~kazsid0_al
 		'http://files.baurzhan.info/inxi_1.7.28-1_all.deb'
 		'http://ppa.launchpad.net/ubuntu-wine/ppa/ubuntu/pool/main/w/wine1.4/wine1.4_1.4-0ubuntu1~ppa2~oneiric3_i386.deb'
 		'http://linukz.org/debian/pool/main/s/stardict-kazrus/stardict-kazrus_20100827-1_all.deb'
-		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flash-properties-gtk_11.2.202.233-0precise1_i386.deb'
-		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flashplugin_11.2.202.233-0precise1_i386.deb'
+		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flash-properties-gtk_11.2.202.235-0precise1_i386.deb'
+		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flashplugin_11.2.202.235-0precise1_i386.deb'
 	)
 
 mkdir $DIRECTORY
