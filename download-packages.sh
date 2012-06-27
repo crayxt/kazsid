5285f640e73d9618a94fdb444bbcae6ed1fd7bad
Baurzhan Muftakhidinov <baurthefirst@gmail.com>
2012-06-27 19:17:48 +0600

    Drop wine from Ubuntu PPA, we are using wine from Debian repos now.

--- download-packages.sh
+++ download-packages.sh
@@ -13,7 +13,6 @@ PACKAGES=(	'http://dl.dropbox.com/u/5626215/live-installer_2012.05.28~kazsid0_al
 		'http://ppa.launchpad.net/webupd8team/themes/ubuntu/pool/main/f/faenza-colors/faenza-cupertino_0.3.4-1~webupd8~oneiric_all.deb'
 		'http://files.baurzhan.info/ttf-microsoft-corefonts_1-1_all.deb'
 		'http://files.baurzhan.info/inxi_1.7.28-1_all.deb'
-		'http://ppa.launchpad.net/ubuntu-wine/ppa/ubuntu/pool/main/w/wine1.4/wine1.4_1.4-0ubuntu1~ppa2~oneiric3_i386.deb'
 		'http://linukz.org/debian/pool/main/s/stardict-kazrus/stardict-kazrus_20100827-1_all.deb'
 		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flash-properties-gtk_11.2.202.236-0precise1_i386.deb'
 		'http://archive.canonical.com/ubuntu/pool/partner/a/adobe-flashplugin/adobe-flashplugin_11.2.202.236-0precise1_i386.deb'
