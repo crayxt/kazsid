db08269b4830010c2cacd2e0ac37e83cbe29bf55
Baurzhan Muftakhidinov <baurthefirst@gmail.com>
2012-10-25 15:02:06 +0600

    Update deadbeef build.

--- download-packages.sh
+++ download-packages.sh
@@ -8,7 +8,7 @@
 
 DIRECTORY="config/packages.chroot"
 PACKAGES=(	'http://dl.dropbox.com/u/5626215/live-installer_2012.10.08~kazsid0_all.deb'
-		'http://dl.dropbox.com/u/5626215/deadbeef_0.5.5-1_i386.deb'
+		'http://dl.dropbox.com/u/5626215/deadbeef_0.5.6-1_i386.deb'
 		'http://ppa.launchpad.net/tiheum/equinox/ubuntu/pool/main/f/faenza-icon-theme/faenza-icon-theme_1.2_all.deb'
 		'http://ppa.launchpad.net/webupd8team/themes/ubuntu/pool/main/f/faenza-colors/faenza-cupertino_0.3.4-1~webupd8~oneiric_all.deb'
 		'http://files.baurzhan.info/ttf-microsoft-corefonts_1-1_all.deb'
