d7bd27d928bf4736ee66eb32340d614a4e8fe8a6
Baurzhan Muftakhidinov <baurthefirst@gmail.com>
2014-09-04 21:48:39 +0600

    Use faenza icons package from Debian repos.

--- download-packages.sh
+++ download-packages.sh
@@ -8,8 +8,6 @@
 
 DIRECTORY="config/packages.chroot"
 PACKAGES=(	'http://dl.dropbox.com/u/5626215/live-installer_2012.11.26~kazsid0_all.deb'
-		'https://dl.dropboxusercontent.com/u/5626215/deadbeef_0.6.0-kazsid0_i386.deb'
-		'http://ppa.launchpad.net/tiheum/equinox/ubuntu/pool/main/f/faenza-icon-theme/faenza-icon-theme_1.3.1_all.deb'
 		'http://ppa.launchpad.net/webupd8team/themes/ubuntu/pool/main/f/faenza-colors/faenza-cupertino_0.3.4-1~webupd8~oneiric_all.deb'
 		'https://www.dropbox.com/s/xd53tet3lzj9l47/ttf-microsoft-corefonts_1-1_all.deb'
 		'https://dl.dropboxusercontent.com/u/5626215/stardict-kazrus_20100827-1_all.deb'
